325dd9c99074162db70df112a8abac1ad9d73dea
chwilko <bartekchwilkowski@gmail.com>
2022-05-27 19:49:31 +0200

    dodane testy

---
+++ tests.py
@@ -0,0 +1,44 @@
+# import pytest
+from WeridText import *
+import os
+example_texts = "example_texts"
+
+
+def test_Constants_index():
+    assert -1 == constants_index("asdasd")
+    assert 0 == constants_index(" asdasd")
+    assert 1 == constants_index("a\nsdasd")
+    assert 4 == constants_index("asda{}sd")
+    assert -1 == constants_index("asdasd ")
+
+def test_Shuffle_world():
+    world = "Parrot"
+    world2 = "Parr"
+    for i in range(10000):
+        assert world != shuffle_word(world)
+        assert world2 != shuffle_word(world2)
+
+def test_To_code():
+    assert to_code("Wenn")
+    assert ~to_code("ist")
+    assert ~to_code("das")
+    assert to_code("Nunstuck")
+    assert ~to_code("git")
+    assert ~to_code("und")
+    assert to_code("slotermeyer")
+    assert ~to_code("Ja")
+    assert to_code("beiherhund")
+    assert ~to_code("das")
+    assert to_code("Oder")
+    assert ~to_code("Die")
+    assert to_code("Flipperwald")
+    assert to_code("gerstuck")
+
+def test_Decode_and_encode():
+    texts = os.listdir(example_texts)
+    for file_text in texts:
+        with open(os.sep.join([example_texts, file_text]), "r") as f:
+            text = f.read()
+        assert text == decode(encode(text))        
+
+
