4532f107a729f918c82877d05931ff473b61fb35
chwilko <bartekchwilkowski@gmail.com>
2022-05-29 21:00:47 +0200

    Add home page, resize textfield

--- v1/urls.py
+++ v1/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.code, name="encode_req"),
-    path('encode', views.encode_run, name="encode"),
-    path('decode', views.decode_run, name="decode"),
+    path('', views.home, name="encode_req"),
+    path('v1/', views.code, name="encode_req"),
+    path('v1/encode', views.encode_run, name="encode"),
+    path('v1/decode', views.decode_run, name="decode"),
 ]
--- v1/views.py
+++ v1/views.py
@@ -1,22 +1,21 @@
 
 import sys
-import os
 from pathlib import Path
 from django.shortcuts import render
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-with open("try.txt", "w") as f:
-    print(BASE_DIR, file=f)
-    print(os.listdir(BASE_DIR), file=f)
+
 sys.path.append(BASE_DIR)
   
-import WeridText as WT
+import WeirdText as WT
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html')
+
 def code(request):
     return render(request, 'code.html')
-
 
 def encode_run(request):
     text = WT.encode(request.GET['text_to_encode'])
@@ -24,5 +23,5 @@ def encode_run(request):
 
 def decode_run(request):
     text = request.GET['text_to_decode']
-    text = WT.decode(text, text_sep = '\r\n—weird—\r\n')
+    text = WT.decode(text, text_sep = '\r\n-weird-\r\n')
     return render(request, 'result.html', {"result" : text })
